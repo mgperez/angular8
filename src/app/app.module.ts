d87830646923c750d488f34dac7ad238ef4a4389
Marcial Garcia <marcial.garciaperez@gmail.com>
2020-12-20 11:02:59 +0100

    clase3-4-ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './components/correo/correo.component';
 import { ListaCorreosComponent } from './components/lista-correos/lista-correos.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ListaCorreosComponent } from './components/lista-correos/lista-correos.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
